5ef489af983c3f47b93dd9462eb938a096983c5c
AlexanderGalimov <alexgalimov2003@mail.ru>
2023-09-27 21:56:50 +0300

    business_logic_change_2

--- src/ru/vsu/cs/galimov/tasks/business_logic/Department.java
+++ src/ru/vsu/cs/galimov/tasks/business_logic/Department.java
@@ -1,14 +1,15 @@
 package ru.vsu.cs.galimov.tasks.business_logic;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Department {
     private final String name;
-    private final List<Double> workingHours;
+    private final List<Date> workingHours;
     private final List<Product> productsInStock = new ArrayList<>();
 
-    public Department(String name, List<Double> workingHours) {
+    public Department(String name, List<Date> workingHours) {
         this.name = name;
         this.workingHours = workingHours;
     }
@@ -17,11 +18,43 @@ public class Department {
         productsInStock.add(product);
     }
 
+    private void removeProduct(Product product) {
+        this.productsInStock.remove(product);
+    }
+
+    private void addDate(Date newDate) {
+        this.workingHours.add(newDate);
+    }
+
+    private void changeDate(Date oldDate, Date newDate) {
+        int index = this.workingHours.indexOf(oldDate);
+        this.workingHours.set(index, newDate);
+    }
+
+    private void removeDate(Date dateToRemove){
+        this.workingHours.remove(dateToRemove);
+    }
+
+    public void showProducts(){
+        if (this.productsInStock.size() == 0){
+            System.out.println("empty department");
+            return;
+        }
+
+        for (Product prod: this.productsInStock) {
+            System.out.println(prod);
+        }
+    }
+
+    public boolean isEmpty(){
+        return this.productsInStock.size() == 0;
+    }
+
     public String getName() {
         return name;
     }
 
-    public List<Double> getWorkingHours() {
+    public List<Date> getWorkingHours() {
         return workingHours;
     }
 
