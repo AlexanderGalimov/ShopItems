388192b53fd104df9548a83b8f560bb130db10ef
AlexanderGalimov <alexgalimov2003@mail.ru>
2023-10-14 20:41:22 +0300

    atta1 refactor

---
+++ src/ru/vsu/cs/galimov/tasks/command/FindProductById.java
@@ -0,0 +1,26 @@
+package ru.vsu.cs.galimov.tasks.command;
+
+import ru.vsu.cs.galimov.tasks.service.ServiceProductImplementation;
+
+import java.util.Scanner;
+
+public class FindProductById extends Command {
+    protected final ServiceProductImplementation serviceProductImplementation;
+
+    public FindProductById(Scanner scanner) {
+        super(scanner);
+        serviceProductImplementation = ServiceProductImplementation.getINSTANCE();
+    }
+
+    @Override
+    public void runCommand() {
+        System.out.print("Enter id product: ");
+        int id = scanner.nextInt();
+        serviceProductImplementation.findById(id);
+    }
+
+    @Override
+    public String getCommandName() {
+        return "Find product by id";
+    }
+}
---
+++ src/ru/vsu/cs/galimov/tasks/service/ServiceProductImplementation.java
@@ -0,0 +1,65 @@
+package ru.vsu.cs.galimov.tasks.service;
+
+import ru.vsu.cs.galimov.tasks.logic.Logic;
+import ru.vsu.cs.galimov.tasks.model_objects.Product;
+import ru.vsu.cs.galimov.tasks.repository.InMemoryRepositoryProductImplementation;
+
+import java.util.List;
+
+public class ServiceProductImplementation implements ProductService{
+    private static ServiceProductImplementation INSTANCE;
+    private final InMemoryRepositoryProductImplementation products;
+
+    public ServiceProductImplementation() {
+        this.products = new InMemoryRepositoryProductImplementation();
+    }
+
+    public static ServiceProductImplementation getINSTANCE() {
+        if (INSTANCE == null) {
+            INSTANCE = new ServiceProductImplementation();
+        }
+        return INSTANCE;
+    }
+
+
+    @Override
+    public void add(Product object) {
+        if (Logic.isInDepartment(object.getDepartmentId())) {
+            products.add(object);
+        } else {
+            throw new IllegalArgumentException("Product dep isn't exist");
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        products.delete(id);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return products.findItemById(id);
+    }
+
+    @Override
+    public void renew(int id, Product object) {
+        if (Logic.isInDepartment(object.getDepartmentId())) {
+            products.renew(id, object);
+        } else {
+            throw new IllegalArgumentException("Product dep isn't exist");
+        }
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return products.findAllItems();
+    }
+
+    public List<Product> allProductInDepartment(int idDep) {
+        if (Logic.isInDepartment(idDep)) {
+            return Logic.allProductInDepartment(idDep);
+        } else {
+            throw new IllegalArgumentException("Product dep isn't exist");
+        }
+    }
+}
